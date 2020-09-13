072b5c03dece6c440525295f7deacf69c04fc91d
AK <64682846+archanakumari-ak@users.noreply.github.com>
2020-09-14 02:25:23 +0530

    deleting todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -14,3 +14,10 @@ $("li").click(function () {
 
 	$(this).toggleClass("completed");
 });
+
+$("span").click(function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation(); 		//prevent bubbling
+});
